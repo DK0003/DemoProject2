e826e7d91a152f329398f0aaaffb8ebb3f28986e
Dinesh Kumar A <dineshkumar.pro2k@gmail.com>
2024-09-01 11:15:52 +0000

    Pending changes exported from your codespace

--- src/main/java/com/project/Demo/Sample.java
+++ src/main/java/com/project/Demo/Sample.java
@@ -3,7 +3,8 @@ package com.project.Demo;
 public class Sample {
     public static void main( String[] args ){
 
+    System.out.println("Hello World!");
+    System.out.println("Won?????????");
     
-    System.out.println("Hi");
     }
 }
